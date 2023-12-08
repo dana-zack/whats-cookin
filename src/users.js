c350dadde3fc0557997d1db85c421c194d42299b
Dana Zack <danaezack@gmail.com>
2023-12-07 19:11:14 -0700

    Refactor: edit sample data again and update across files

---
+++ src/users.js
@@ -0,0 +1,27 @@
+function addUserRecipe(user, recipe) {
+  user.recipesToCook.push(recipe);
+  return user;
+}
+
+function removeUserRecipe(user, recipe) {
+  const index = user.recipesToCook.indexOf(recipe)
+  console.log(index)
+  user.recipesToCook.splice(index, 1)
+  console.log(user)
+  return user
+}
+
+function filterUserRecipesByTag() {
+
+}
+
+function filterUserRecipesByName() {
+
+}
+
+module.exports = {
+  addUserRecipe,
+  removeUserRecipe,
+  filterUserRecipesByTag,
+  filterUserRecipesByName
+};
