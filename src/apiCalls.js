436ea445aa1536aacaf56f37ad6e9cb950d3103b
Ricky Tran <therichardtran@gmail.com>
2024-01-03 08:49:16 -0700

    Fix: refactor API functions to make one function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,30 +1,8 @@
-// Your fetch requests will live here!
-
-console.log('I will be a fetch request!')
-
-function fetchUsers() {
-   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
-    .then(response => response.json())
-    // .then(data => data)
-    .catch(err => console.log('error'))
+export function getData(url) {
+  const data = fetch(url)
+  .then(response => response.json())
+  .catch(err => console.log('error', err))
+  console.log(data);
+  return data
 }
 
-function fetchRecipes() {
-  const recipes = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
-    .then(response => response.json())
-    // .then(data => data)
-    .catch(err => console.log('error'))
-  return recipes;
-}
-
-function fetchIngredients() {
-  const ingredients = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
-    .then(response => response.json())
-    // .then(data => data)
-    .catch(err => console.log('error'))
-  return ingredients;
-}
-
-const fetchedPromises = () => Promise.all([fetchUsers(), fetchRecipes(), fetchIngredients()])
-
-export { fetchedPromises }
