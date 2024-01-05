260657a29388556c712927d57ca3f2a59e240877
Ricky Tran <therichardtran@gmail.com>
2024-01-04 20:43:13 -0700

    Feat: Implement backend API for POST requests

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,6 @@
-//NOTE: Data model and non-dom manipulating logic will live in this file.
-
 import './styles.css'
 import apiCalls from './apiCalls'
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-// Below are examples of how you can import functions from either the recipes or domUpdates files.
 import { getUsers, getRecipes, getIngredients, onLoad, closeModal, displayRecipeCards, updateCurrentRecipe, displayModal, displayRecipesByTag, displayRecipesByName } from './domUpdates';
 import { filterByTag, filterByName, listRecipeIngredients, calculateRecipeCost, getInstructions } from './recipes.js';
 import { addFavoriteRecipe, removeFavoriteRecipe, getRandomIndex, getRandomUser } from './users.js';
@@ -12,7 +8,3 @@ import { fetchUsers, fetchRecipes, fetchIngredients }from './apiCalls'
 import apiRecipes from './domUpdates'
 import apiIngredients from './domUpdates'
 import apiUsers from './domUpdates'
-
-// import ingredientsData from './data/ingredients'
-// import recipeData from './data/recipes'
-// import usersData from './data/users'
